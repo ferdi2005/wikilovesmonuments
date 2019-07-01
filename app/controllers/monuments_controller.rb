1faa342c1a73c9f8b23aa857242ddb1b3e6578f5
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-07-01 12:18:13 +0200

    Mappa generale in index, calcolo del geocenter, api mapbox

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -1,12 +1,16 @@
 class MonumentsController < ApplicationController
   include Pagy::Backend
+  include MonumentsHelper
 
   def index
     if params[:latitude] && params[:longitude]
       @pagy, @monument = pagy(Monument.near([BigDecimal.new(params[:latitude]), BigDecimal.new(params[:longitude])]))
+      @monument_nopagy = Monument.near([BigDecimal.new(params[:latitude]), BigDecimal.new(params[:longitude])])
     elsif params[:city]
       @pagy, @monument = pagy(Monument.near("#{params[:city]}"))
+      @monument_nopagy = Monument.near(params[:city])
     end
+    @geocenter = Geocoder::Calculations.geographic_center(@monument_nopagy)
   end
 
   def show
--- app/helpers/monuments_helper.rb
+++ app/helpers/monuments_helper.rb
@@ -1,3 +1,6 @@
 module MonumentsHelper
   include Pagy::Frontend
+  def geocenter(monuments)
+    return Geocoder::Calculations.geographic_center(monuments)  
+  end 
 end
