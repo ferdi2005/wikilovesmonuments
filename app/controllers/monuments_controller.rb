eccc5146959baa90eaf79965eb2ee2f399221661
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-07-29 11:43:39 +0200

    Centro della mappa a dove si trova l'utente, marker utente

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -6,11 +6,13 @@ class MonumentsController < ApplicationController
     if params[:latitude] && params[:longitude]
       @pagy, @monument = pagy(Monument.near([BigDecimal.new(params[:latitude]), BigDecimal.new(params[:longitude])]))
       @monument_nopagy = Monument.near([BigDecimal.new(params[:latitude]), BigDecimal.new(params[:longitude])])
+      @geocenter = [params[:latitude].to_i, params[:longitude].to_i]
     elsif params[:city]
       @pagy, @monument = pagy(Monument.near("#{params[:city]}"))
       @monument_nopagy = Monument.near(params[:city])
+      result = Geocoder.search(params[:city])
+      @geocenter = result.first.coordinates
     end
-    @geocenter = Geocoder::Calculations.geographic_center(@monument_nopagy)
   end
 
   def show
