bd030775d6305a40b3a71d7cadfbc1094990b057
Ferdinando Traversa <ferdi.traversa@gmail.com>
2020-07-07 12:15:46 +0200

    Esclude i monumenti che abbiano già un'immagine salvata

--- app/jobs/lookup_job.rb
+++ app/jobs/lookup_job.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
 
 class LookupJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    Monument.all.each do |monument|
+  def perform(*_args)
+    Monument.where(image: nil).each do |monument|
       search = '"' + monument.wlmid + '"'
       retimes = 0
       begin
-        count = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&list=search&srsearch=#{search}&srwhat=text&srnamespace=6&srlimit=1&format=json", uri_adapter: Addressable::URI).to_a
-      rescue
+        count = HTTParty.get("https://commons.wikimedia.org/w/api.php?action=query&list=search&srsearch=#{search}&srwhat=text&srnamespace=6&srlimit=1&format=json",
+                             headers: { 'User-Agent' => 'WikiLovesMonumentsItaly MonumentsFinder/1.4 (https://github.com/ferdi2005/wikilovesmonuments; ferdi.traversa@gmail.com) using HTTParty Ruby Gem' },
+                             uri_adapter: Addressable::URI).to_a
+      rescue StandardError
         retimes += 1
         if retimes < 4
           puts "Retrying for ID: #{monument.id} - WLMID: #{monument.wlmid}"
@@ -18,12 +21,12 @@ class LookupJob < ApplicationJob
           next
         end
       end
-      if (count[1][0] == "continue")
-        totalhits = count[2][1]['searchinfo']['totalhits']
-      else
-        totalhits = count[1][1]['searchinfo']['totalhits']
-      end
-      if (totalhits > 0)
+      totalhits = if count[1][0] == 'continue'
+                    count[2][1]['searchinfo']['totalhits']
+                  else
+                    count[1][1]['searchinfo']['totalhits']
+                  end
+      if totalhits > 0
         monument.update_attributes(with_photos: true, photos_count: totalhits)
       else
         monument.update_attributes(with_photos: false, photos_count: totalhits)
