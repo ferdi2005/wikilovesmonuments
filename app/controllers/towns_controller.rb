6e86ecd29d1a5bf21de7a39302e73e448babaf71
ferdi2005 <ferdi.traversa@gmail.com>
2021-07-20 21:20:07 +0200

    Ricerca in inglese delle città

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,7 +1,12 @@
 class TownsController < ApplicationController
   def search
+    if params[:locale] == "it"
+      @towns = Town.search(params[:query], :it)
+    else
+      @towns = Town.search(params[:query], :en)
+    end
     respond_to do |format|
-      format.json { render :json => Town.search(params[:query])}
+      format.json { render :json => @towns}
     end
   end
 end
--- app/models/town.rb
+++ app/models/town.rb
@@ -2,7 +2,11 @@ class Town < ApplicationRecord
     validates :name, uniqueness: { scope: :disambiguation }
     reverse_geocoded_by :latitude, :longitude
 
-    def self.search(query = '')
-        where('lower(name) LIKE lower(?)', "#{query.strip}%")
+    def self.search(query = '', locale = :en)
+        if locale == :it
+            where('lower(name) LIKE lower(?)', "#{query.strip}%")
+        else
+            where('lower(english_name) LIKE lower(?)', "#{query.strip}%")
+        end
     end    
 end
