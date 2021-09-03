c58b74b1c2dba2f0703a980ce6a97b5999597d38
ferdi2005 <ferdi.traversa@gmail.com>
2021-09-04 01:00:10 +0200

    Velocizzo di molto la ricerca dei duplicati

--- app/jobs/check_duplicates_job.rb
+++ app/jobs/check_duplicates_job.rb
@@ -2,12 +2,10 @@ class CheckDuplicatesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Monument.all.each do |mon|
-      if Monument.where(wlmid: mon.wlmid).count > 1
-        mon.update(duplicate: true)
-      else
-        mon.update(duplicate: false)
-      end
-    end
+    duplicati = Monument.pluck(:id, :wlmid).group_by { |m| m[1] }.delete_if { |_, k| k.count == 1 }
+    to_be_updated = {}
+    Monument.update_all(duplicate: false)
+    duplicati.each { |_, d| d.each { |t| to_be_updated[t[0]] = {"duplicate": true } }}
+    Monument.update(to_be_updated.keys, to_be_updated.values)
   end
 end
