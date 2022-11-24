a68046acefc8e8aa123e6e53cb51d71aafb8e3bf
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-11-24 19:51:53 +0100

    Gestisco rigenerazione della cache

--- app/jobs/cache_warm_job.rb
+++ app/jobs/cache_warm_job.rb
@@ -1,9 +1,34 @@
 class CacheWarmJob < ApplicationJob
   queue_as :default
+  
+  include ActionDispatch::Integration::RequestHelpers
 
   def perform(*args)
+    @regioni = ["Marche",                                     
+      "Lombardia",                                  
+      "Piemonte",                                   
+      "Liguria",                                    
+      "Sicilia",                                    
+      "Lazio",                                      
+      "Campania",                                   
+      "Basilicata",                                 
+      "Abruzzo",                                    
+      "Emilia-Romagna",                             
+      "Puglia",                                     
+      "Umbria",                                     
+      "Toscana",                                    
+      "Valle d'Aosta",                              
+      "Friuli-Venezia Giulia",
+      "Sardegna",
+      "Molise",
+      "Veneto",
+      "Calabria",
+      "Trentino-Alto Adige"]  
+
+    @monuments = Monument.where(hidden: false)
+
     MonumentsController.expire_page("map")
 
-    app.get("/map")
+    MonumentsController.cache_page(MonumentsController.render(:map, assigns: {regioni: @regioni, monuments: @monuments} ), "map")
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+ActiveSupport::Reloader.to_prepare do
+    ApplicationController.renderer.defaults.merge!(
+    http_host: Rails.application.routes.default_url_options[:host],
+    https: true
+    )
+end
