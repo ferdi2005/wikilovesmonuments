44452e8d97dc1a9405f0b4c37aea27664076ce26
Ferdinando Traversa <ferdi.traversa@gmail.com>
2026-09-07 16:58:40 +0200

    capistrano aggiornato

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock "~> 3.20.1"
 
 set :application, "wikilovesmonuments"
 
@@ -22,7 +22,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log,  "#{release_path}/log/puma.error.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_phased_restart, true
